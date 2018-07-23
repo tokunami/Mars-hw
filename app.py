2c17d899c02d5c31a9f9490133b0d06c077a3fca
tokunami <nami.tokunaga@fujixerox.co.jp>
2018-07-23 12:51:34 -0700

    add scripts

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
+mongo = PyMongo(app)
+
+
+@app.route('/')
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update(
+        {},
+        mars_data,
+        upsert=True
+    )
+    # return 'Scraping Successful!'
+    return redirect('http://localhost:5000')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
